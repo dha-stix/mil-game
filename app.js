2371179dc45417c7d590ae0fe3b8104fe10b7256
dha-stix <asaoludavid234@yahoo.com>
2020-12-04 03:25:08 +0100

    Finished

---
+++ app.js
@@ -0,0 +1,44 @@
+//REPRESENT ELEMENTS AS VARIABLES
+const button = document.querySelector(".checkBtn")
+const inputValue = document.querySelector(".input")
+const computerGuess =  Math.floor(Math.random() * 100)
+const emptyAlert = document.querySelector(".alert-empty")
+const rangeErrorAlert = document.querySelector(".alert-range")
+const hotAlert = document.querySelector(".alert-info")
+const coldAlert = document.querySelector(".alert-cold")
+const normalAlert = document.querySelector(".alert-normal")
+
+//FUNCTIONS
+const checkTemperature = (userGuess, computerGuess) => {
+    if(userGuess > computerGuess) {
+        hotAlert.classList.replace("alert", "show")
+    }
+    else if(userGuess === computerGuess) {
+       normalAlert.classList.replace("alert", "show")
+    }
+    else {
+        coldAlert.classList.replace("alert", "show")
+    }
+}
+
+
+//EVENTS
+button.addEventListener("click", (e)=> {
+    e.preventDefault()
+   if(!inputValue.value) {
+       emptyAlert.classList.replace("alert", "show")
+   }
+   else if(inputValue.value < 0  || inputValue.value > 100 ) {
+    rangeErrorAlert.classList.replace("alert", "show")
+   }
+   else {
+    checkTemperature(inputValue.value, computerGuess)
+   }
+   inputValue.value = null
+})
+
+
+    
+
+
+
